f864143e9fad05d5d9b0ac1a66ce629c7a2ea181
vmakams <vmakam@aum.edu>
2023-11-30 01:53:11 -0500

    Java Sample first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Employee Information");
+
+        EmployeeInfo ei = new EmployeeInfo();
+        ei.employeeInfo();
+
     }
 }
